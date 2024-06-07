f4edb76e79a72f9ef2f8803efed2279eeaf8a855
Phan Trung Dũng <aninatsumi@gmail.com>
2024-06-07 15:48:19 +0700

    add: lab1

--- lab1/ProductManagement/Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ lab1/ProductManagement/Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bc1295b7c4781a77fc46b2ea4136fda9d5229af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
